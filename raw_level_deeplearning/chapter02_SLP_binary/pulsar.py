9ba96ea7ba50f2e0d465bbe5598f4922dfae81f8
Junho Yun <43261253+philgineer@users.noreply.github.com>
2021-03-04 02:31:55 +0900

    update

--- raw_level_deeplearning/chapter02_SLP_binary/pulsar.py
+++ raw_level_deeplearning/chapter02_SLP_binary/pulsar.py
@@ -11,23 +11,39 @@ RND_STD = 0.03
 LEARNING_RATE = 0.01
 
 
-def pulsar_exec(epoch_count=10, mb_size=10, report=1):
-    load_pulsar_dataset()
+def pulsar_exec(epoch_count=20, mb_size=10, report=2, adjust_ratio=True):
+    load_pulsar_dataset(adjust_ratio) # adjust_ratio for recall(재현율)
     init_model()
     train_and_test(epoch_count, mb_size, report, show=True)
 
 
-def load_pulsar_dataset():
+def load_pulsar_dataset(adjust_ratio):
+    pulsars, stars = [], []
     with open('../../../../data/raw_level/pulsar_stars.csv') as csvfile:
         csvreader = csv.reader(csvfile)
         next(csvreader, None)
         rows = []
         for row in csvreader:
-            rows.append(row)
+            if row[8] == '1':
+                pulsars.append(row)
+            else:
+                stars.append(row)
 
     global data, input_cnt, output_cnt
     input_cnt, output_cnt = 8, 1
-    data = np.asarray(rows, dtype='float32')
+
+    star_cnt, pulsar_cnt = len(stars), len(pulsars)
+
+    if adjust_ratio:
+        data = np.zeros([2 * star_cnt, 9])
+        data[0:star_cnt, :] = np.asarray(stars, dtype='float32')
+        for n in range(star_cnt):
+            data[star_cnt + n] = np.asarray(pulsars[n % pulsar_cnt], dtype='float32')
+
+    else:
+        data = np.zeros([star_cnt + pulsar_cnt, 9])
+        data[0:star_cnt, :] = np.asarray(stars, dtype='float32')
+        data[star_cnt:, :] = np.asarray(pulsars, dtype='float32')
 
 
 def init_model():
@@ -41,23 +57,26 @@ def train_and_test(epoch_count, mb_size, report, show=False):
     test_x, test_y = get_test_data()
 
     for epoch in range(epoch_count):
-        losses, accs = [], []
+        losses = []
 
         for n in range(step_count):
             train_x, train_y = get_train_data(mb_size, n)
-            loss, acc = run_train(train_x, train_y)
+            loss, _ = run_train(train_x, train_y)
             losses.append(loss)
-            accs.append(acc)
 
         if report > 0 and (epoch+1) % report == 0:
             acc = run_test(test_x, test_y)
+            acc_str = ' | '.join(['%5.3f']*4) % tuple(acc)
             if show==True:
-                print('Epoch {}: loss={:5.3f}, acc={:5.3f}/{:5.3f}'.
-                format(epoch+1, np.mean(losses), np.mean(accs), acc))
+                if (epoch == 0):
+                    print('                            | ACC  | PREC  | RECAL | F1  |')
+                print('Epoch {:2d}: loss={:5.3f}, result={}'.
+                format(epoch+1, np.mean(losses), acc_str))
 
-    final_acc = run_test(test_x, test_y)
+    acc = run_test(test_x, test_y)
+    acc_str = ' | '.join(['%5.3f']*4) % tuple(acc)
     if show==True:
-        print('\nFinal Test: acc = {:5.3f}'.format(final_acc))
+        print('\nFinal Test: final scores = {}'.format(acc_str))
 
 
 def arrange_data(mb_size):
@@ -144,11 +163,30 @@ def backprop_postproc(G_loss, aux):
 
 
 def eval_accuracy(output, y):
-    estimate = np.greater(output, 0)
-    answer = np.greater(y, 0.5)
-    correct = np.equal(estimate, answer)
+    est_yes = np.greater(output, 0)
+    ans_yes = np.greater(y, 0.5)
+    est_no = np.logical_not(est_yes)
+    ans_no = np.logical_not(ans_yes)
 
-    return np.mean(correct)
+    tp = np.sum(np.logical_and(est_yes, ans_yes))
+    fp = np.sum(np.logical_and(est_yes, ans_no))
+    tn = np.sum(np.logical_and(est_no, ans_no))
+    fn = np.sum(np.logical_and(est_no, ans_yes))
+
+    acc = safe_div(tp + tn, tp + fp + tn + fn)
+    precision = safe_div(tp, tp + fp)
+    recall = safe_div(tp, tp + fn)
+    f1 = 2 * safe_div(recall * precision, recall + precision)
+
+    return [acc, precision, recall, f1]
+
+
+def safe_div(p, q):
+    p, q = float(p), float(q)
+    if np.abs(q) < 1.0e-20:
+        return np.sign(p)
+
+    return p / q
 
 
 def relu(x):
